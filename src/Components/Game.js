7f07736f100c0ccd2b6ccb37dfe9854c5af0f59f
Billy Lam <3700437+billylam@users.noreply.github.com>
2022-01-20 22:49:08 -0800

    Add square colors

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,20 +1,21 @@
 import React, {Component} from 'react';
-import Grid from './Grid'
 import Word from './Word'
 export default class Game extends Component {
   constructor(props) {
     super(props)
     this.state = {
       guesses: [],
+      colors: [],
       currentGuess: '',
       isWinner: false,
       isLoser: false,
-      target: 'WINNR',
-      disabled: false,
+      target: 'HELLO',
+      guessedLetters: {},
     }
 
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.colorize = this.colorize.bind(this)
   }
   
   handleChange(e) {
@@ -29,9 +30,9 @@ export default class Game extends Component {
       return;
     }
 
-
     const guesses = this.state.guesses.slice()
     this.setState({guesses: guesses.concat([this.state.currentGuess]), currentGuess: ''})
+    this.colorize()
     if (this.state.currentGuess === this.state.target){
       this.setState({isWinner: true})
     }
@@ -40,11 +41,51 @@ export default class Game extends Component {
     }
   }
 
+  colorize() {
+    // Rules: 
+    //   First hash count of each letter in target
+    const counts = {};
+    if (!this.state.targetLetterCounts) {
+      for (let i = 0; i < this.state.target.length; i++) {
+        if (counts[this.state.target[i]]) {
+          counts[this.state.target[i]] = counts[this.state.target[i]] + 1
+        }
+        else counts[this.state.target[i]] = 1
+      }
+    }
+    
+    //   Iterate through each guess's letters twice
+    //   Handle exact matches first and color green, decrement if found
+    const colors = Array(this.state.target.length).fill('')
+    for (let i = 0; i < this.state.currentGuess.length; i++) {
+      if (this.state.currentGuess[i] === this.state.target[i]) {
+        colors[i] = 'G'
+        counts[this.state.currentGuess[i]] = counts[this.state.currentGuess[i]] - 1
+      }
+    }
+    //   Handle rest
+    for (let i = 0; i < this.state.currentGuess.length; i++) {
+      if (this.state.currentGuess[i] !== this.state.target[i]) {
+        if (counts[this.state.currentGuess[i]] > 0) {
+          colors[i] = 'Y'
+          counts[this.state.currentGuess[i]] = counts[this.state.currentGuess[i]] - 1
+        }
+        else colors[i] = 'R'
+      }
+    }
+    console.log(colors)
+    //   Join string and pass to state
+    this.setState({colors: this.state.colors.slice().concat([colors.join('')])})
+  }
+
   render() {
-    const words = this.state.guesses.map((guess) => <Word />)
+    const words = this.state.guesses.map((guess, i) => <div><Word word={guess} colors={this.state.colors[i]} /></div>)
+    const numBlanks = 6 - this.state.guesses.length
+    const blanks = Array(numBlanks).fill(null).map(() => <div><Word word={Array(this.state.target.length).fill(' ').join('')} colors={Array(this.state.target.length).fill('R').join('')} /></div>)
+
     return (<div>
-      <Grid guesses={this.state.guesses} />
-      <Word />
+      <div>{words}
+      {blanks}</div>
       <form onSubmit={this.handleSubmit}>
         <input id="guess" maxLength='5' disabled={(this.state.isLoser || this.state.isWinner)} onChange={this.handleChange} value={this.state.currentGuess} />
       </form>
--- src/Components/Word.js
+++ src/Components/Word.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 
 function Square(props) {
   return (
-      <button className="square">
+      <button className={"square " + (props.color === 'G' ? 'correct ' : '') + (props.color === 'Y' ? 'almost' : '')}>
         {props.letter}
       </button>
   );
@@ -10,7 +10,9 @@ function Square(props) {
 
 export default class Word extends Component {
   render() {
-    const word = this.props.word?.split('').map((letter) => <Square letter={letter} />)
+    const colors = this.props.colors?.split('')
+    console.log(colors)
+    const word = this.props.word?.split('').map((letter, i) => <Square letter={letter} color={colors[i]} />)
     return (<div>{word}</div>)
   }
 }
